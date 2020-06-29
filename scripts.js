0f021d9f75377f61d60f9b459c7c27ca5afff383
Daniel <dannygulland@gmail.com>
2020-06-29 16:04:55 -0700

    promise.all implementaton

--- scripts.js
+++ scripts.js
@@ -1,37 +1,70 @@
 const form = document.querySelector('.search-form');
-    const text = document.querySelector('.form-control');
-    const characters = [];
-    let allData = [];
-    let dataItems;
-    let count = 2;
-    let index = 1;
+const text = document.querySelector('.form-control');
+const characters = [];
+let allData = [];
+let dataItems;
+let count = 2;
+let index = 1;
+let index2 = 1;
 
-    function getCharacter(name) {
+let callAgain = true;
+// function getCharacter(name, index) {
 
-        //might just be better to type out all the pages instead of using a loop. Basically the same thing
-        allData[0] = fetch(`https://swapi.dev/api/people/`);
-        console.log(allData[0]);
+//     //might just be better to type out all the pages instead of using a loop. Basically the same thing
 
-        for(let i = 0; i < 30; i+=10) {
-            allData[index] = fetch(`https://swapi.dev/api/people/?page=${count}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(values => {
-                for(let i = 0; i < values.results.length; i++) {
-                    if(values.results[i].name == name) {
-                        console.log("True");
-                    }
-                }
-            })
-            count++;
-            index++;
-        }
+//     fetch(`https://swapi.dev/api/people/?page=1`)
+//     .then(blob => blob.json())
+//     .then(data => {
+//         console.log(data);
+//         characters.push(...data.results);
+//         if(data.next === null) {
+//             return;
+//         }
+//     })
+//     .catch(error => console.log(error));
+
+//     index++;
+
+//     console.log(characters);
+//     // getCharacter(name, index++);
+    
+// }
+// function search(e) {
+//     e.preventDefault();
+//     console.log(text.value);
+//     getCharacter(text.value, index);
+// }
+
+// const result = form.addEventListener('submit', search);
+
+let bool = true;
+function getAllRequests() {
+    const requests = [];
+    for(let i = 1; i < 10; i++) {
+        requests.push(fetch('https://swapi.dev/api/people/?page=' + i));
     }
-    function search(e) {
-        e.preventDefault();
-        console.log(text.value);
-        getCharacter(text.value);
-    }
+    
 
-    const result = form.addEventListener('submit', search);
+    console.log(requests);
+    
+    return requests;   
+}
+
+function foo(index) {
+
+    Promise.all(getAllRequests())
+    .then(response => {
+        console.log(response);
+        let x = response.map(res => res.json());
+        console.log(x);
+        return Promise.all(x)
+    })
+    .then(data => {
+        console.log(data);
+        data.forEach(object => characters.push(...object.results));
+    });
+
+    console.log(characters);
+}
+
+const result = form.addEventListener('submit', foo(5));
