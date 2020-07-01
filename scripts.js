41ab49dad9ec797eebba3ad7759a1a999ebd8036
Daniel <dannygulland@gmail.com>
2020-06-30 18:19:15 -0700

    await version of getAllRequests

--- scripts.js
+++ scripts.js
@@ -8,63 +8,50 @@ let index = 1;
 let index2 = 1;
 
 let callAgain = true;
-// function getCharacter(name, index) {
 
-//     //might just be better to type out all the pages instead of using a loop. Basically the same thing
+const requests = [];
+var url;
 
-//     fetch(`https://swapi.dev/api/people/?page=1`)
-//     .then(blob => blob.json())
-//     .then(data => {
-//         console.log(data);
-//         characters.push(...data.results);
-//         if(data.next === null) {
-//             return;
-//         }
-//     })
-//     .catch(error => console.log(error));
 
-//     index++;
-
-//     console.log(characters);
-//     // getCharacter(name, index++);
-    
-// }
-// function search(e) {
-//     e.preventDefault();
-//     console.log(text.value);
-//     getCharacter(text.value, index);
-// }
-
-// const result = form.addEventListener('submit', search);
-
-let bool = true;
-function getAllRequests() {
-    const requests = [];
-    for(let i = 1; i < 10; i++) {
-        requests.push(fetch('https://swapi.dev/api/people/?page=' + i));
+async function getAllRequests(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        url = data.next;
+        characters.push(...data.results);
+        if(url !== null) {
+            return getAllRequests(url);
+        }
     }
-    
-
-    console.log(requests);
-    
-    return requests;   
+    catch(err) {
+        console.log(err);
+    }
 }
 
-function foo(index) {
-
-    Promise.all(getAllRequests())
-    .then(response => {
-        console.log(response);
-        let x = response.map(res => res.json());
-        console.log(x);
-        return Promise.all(x)
-    })
-    .then(data => {
-        console.log(data);
-        data.forEach(object => characters.push(...object.results));
-    });
-
+async function foo(index) {
+    let url = 'https://swapi.dev/api/people/?page=1';
+    getAllRequests(url);
     console.log(characters);
 }
+
+
+
+// Less efficient way..because we're hard coding the number of pages
+
+// function getAllRequests(url) {
+//     for(let i = 1; i < 10; i++) {
+//         requests.push(fetch('https://swapi.dev/api/people/?page=' + i));
+//     }
+//     return requests;   
+// }
+
+// async function foo(index) {
+//     const responses = await Promise.all(getAllRequests());
+//     const data = await Promise.all(responses.map(res => res.json()));
+//     data.forEach(object => characters.push(...object.results));
+
+//     console.log(characters)
+// }
+
 
 const result = form.addEventListener('submit', foo(5));
