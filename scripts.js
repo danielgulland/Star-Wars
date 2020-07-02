07da03662c91b00686c3103437e75fa6236dc2fd
danielgulland <48257522+danielgulland@users.noreply.github.com>
2020-07-01 18:38:03 -0700

    nav bar (#2)
    * promise.all implementaton
    
    * await version of getAllRequests
    
    * navbar
    
    * navbar stoof
    
    Co-authored-by: Nathan Smith <nathan@Nathans-MacBook-Pro.local>

--- scripts.js
+++ scripts.js
@@ -1,37 +1,80 @@
 const form = document.querySelector('.search-form');
-    const text = document.querySelector('.form-control');
-    const characters = [];
-    let allData = [];
-    let dataItems;
-    let count = 2;
-    let index = 1;
+const text = document.querySelector('.form-control');
+const characters = [];
+let allData = [];
+let dataItems;
+let count = 2;
+let index = 1;
+let index2 = 1;
 
-    function getCharacter(name) {
+let callAgain = true;
 
-        //might just be better to type out all the pages instead of using a loop. Basically the same thing
-        allData[0] = fetch(`https://swapi.dev/api/people/`);
-        console.log(allData[0]);
+const requests = [];
+var url;
 
-        for(let i = 0; i < 30; i+=10) {
-            allData[index] = fetch(`https://swapi.dev/api/people/?page=${count}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(values => {
-                for(let i = 0; i < values.results.length; i++) {
-                    if(values.results[i].name == name) {
-                        console.log("True");
-                    }
-                }
-            })
-            count++;
-            index++;
+
+async function getAllRequests(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        url = data.next;
+        characters.push(...data.results);
+        if(url !== null) {
+            return getAllRequests(url);
         }
     }
-    function search(e) {
-        e.preventDefault();
-        console.log(text.value);
-        getCharacter(text.value);
+    catch(err) {
+        console.log(err);
     }
+}
 
-    const result = form.addEventListener('submit', search);
+async function foo(index) {
+    let url = 'https://swapi.dev/api/people/?page=1';
+    await getAllRequests(url);
+    console.log(characters);
+}
+
+// const result = form.addEventListener('submit', foo(5));
+
+// Less efficient way..because we're hard coding the number of pages
+
+// function getAllRequests(url) {
+//     for(let i = 1; i < 10; i++) {
+//         requests.push(fetch('https://swapi.dev/api/people/?page=' + i));
+//     }
+//     return requests;   
+// }
+
+// async function foo(index) {
+//     const responses = await Promise.all(getAllRequests());
+//     const data = await Promise.all(responses.map(res => res.json()));
+//     data.forEach(object => characters.push(...object.results));
+
+//     console.log(characters)
+// }
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+        
+            if(link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+const app = ()=> {
+    navSlide();
+}
+
+app();
