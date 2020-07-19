daddc8706d9db8268ad6f78def058abcbc8bd713
danielgulland <48257522+danielgulland@users.noreply.github.com>
2020-07-19 14:33:36 -0700

    intro to character filtering (#11)
    * promise.all implementaton
    
    * await version of getAllRequests
    
    * navbar
    
    * navbar stoof
    
    * yes
    
    * first attempt at name filtering
    
    * character filtering
    
    * first attempt at name filtering
    
    * promise.all implementaton
    
    * await version of getAllRequests
    
    * navbar
    
    * navbar stoof
    
    * merged dans files
    
    Co-authored-by: Nathan Smith <nathan@Nathans-MacBook-Pro.local>

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-const nav = document.querySelector('nav')
-const form = document.querySelector('.search-form');
-const text = document.querySelector('.form-control');
-const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector("nav");
+const form = document.querySelector(".search-form");
+const text = document.querySelector(".form-control");
+const navLinks = document.querySelector(".nav-links");
+const suggestions = document.querySelector(".suggestions");
+
 const characters = [];
 let films = [];
 let allData = [];
@@ -16,64 +18,89 @@ const requests = [];
 var url;
 
 async function getAllCharacters(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        url = data.next;
-        characters.push(...data.results);
-        if(url !== null) {
-            return getAllCharacters(url);
-        }
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    url = data.next;
+    characters.push(...data.results);
+    if (url !== null) {
+      return getAllCharacters(url);
     }
-    catch(err) {
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function foo() {
-    let url = 'https://swapi.dev/api/people/?page=1';
-    await getAllCharacters(url);
-    console.log(characters);
+  let url = "https://swapi.dev/api/people/?page=1";
+  await getAllCharacters(url);
+  console.log(characters);
 }
 
 async function getAllFilms() {
-    try {
-        const response = await fetch('https://swapi.dev/api/films/')
-        const data = await response.json();
-        films.push(...data.results);
-        console.log(films);
-    }
-    catch(err) {
-        console.log(err);
-    }
+  try {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    films.push(...data.results);
+    console.log(films);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
- const result = form.addEventListener('submit', foo());
- const links = navLinks.addEventListener('click', getAllFilms);
- console.log(nav);
+const result = form.addEventListener("submit", foo());
+const links = navLinks.addEventListener("click", getAllFilms);
+console.log(nav);
 
- const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-        
-            if(link.style.animation) {
-                link.style.animation = '';
-            }
-            else {
-                link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.1s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
     });
+    burger.classList.toggle("toggle");
+  });
+};
 
+function findMatches(value) {
+  console.log(value);
+  return characters.filter((character) => {
+    const regex = new RegExp(value, "gi");
+    return character.name.match(regex);
+  });
 }
 
-const app = ()=> {
-    navSlide();
+function displayMatches() {
+  const matches = findMatches(this.value);
+  const html = matches
+    .map((match) => {
+      console.log(match.name);
+      return `
+        <li>
+        <span class="name">${match.name}</span>
+      </li>`;
+    })
+    .join("");
+  console.log(html);
+  suggestions.innerHTML = html;
 }
+
+const app = () => {
+  navSlide();
+};
+
+suggestions.html = "<h1>hey</h1>";
+
+text.addEventListener("change", displayMatches);
+text.addEventListener("keyup", displayMatches);
 
 app();
